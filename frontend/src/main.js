7b158f776de9f6201ad3b6092a69ccbfeddbbc00
RODRIGUEZ SINCLAIR <juandavidrodriguezsinclair@gmail.com>
2025-01-13 11:48:18 +0100

    Changement de l'application front-end : ajout de MDI pour la visualisation des icônes dans l'arborescence.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,10 +6,14 @@ import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import './assets/css/global.css';  
+import '@mdi/font/css/materialdesignicons.css' // Ajout de cette ligne pour MDI
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi', // Définir MDI comme jeu d'icônes par défaut
+  },
 });
 
 const app = createApp(App);
@@ -22,7 +26,6 @@ app.use(vuetify);
 
 // Fournir Vuetify à l'application
 app.provide('vuetify', vuetify);
-
 
 // Montez l'application
 app.mount('#app');
