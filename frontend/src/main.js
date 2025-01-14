aac1824c72a533581ef175ec9db2502dc6b6f3e5
RODRIGUEZ SINCLAIR <juandavidrodriguezsinclair@gmail.com>
2025-01-14 12:44:43 +0100

    Ajout de Vuex et du store à l'application

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,28 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store'; 
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import './assets/css/global.css';  
-import '@mdi/font/css/materialdesignicons.css' // Ajout de cette ligne pour MDI
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
   components,
   directives,
   icons: {
-    defaultSet: 'mdi', // Définir MDI comme jeu d'icônes par défaut
+    defaultSet: 'mdi',
   },
 });
 
 const app = createApp(App);
 
-// Utilisez le routeur avant Vuetify
+// Utilisez le store Vuex
+app.use(store); 
+
+// Utilisez le routeur
 app.use(router);
 
 // Utilisez Vuetify
